df79e6497503777fc6198c97f671999b390ac62c
ViegasAdilson <adilsonviegas@hotmail.com>
2022-10-11 07:13:31 +0100

    image

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
 from flask import Flask, render_template
+from flask_wtf.file import FileField, FileRequired
+from werkzeug.utils import secure_filename
+from flask_wtf import FlaskForm
+import os
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '300922'
 
 
-@app.route('/')
+class PhotoForm(FlaskForm):
+    photo = FileField(validators=[FileRequired()])
+
+
+@app.route('/', methods=['GET', 'POST'])
 def home_page():
-    return render_template('index.html')
+    form = PhotoForm()
+    f = 'static/Images/download.jpeg'
+    filename = secure_filename(f.filename)
+    f.save(os.path.join(
+        app.instance_path, 'static/Images', filename
+    ))
+    return render_template('index.html', form=form)
 
 
 if __name__ == '__main__':
